d6caf3a9e1bc3ca3536b7951fb4aa1e20023550a
David Thornley <david.thornley@touchstargroup.com>
2016-09-06 10:56:18 +1000

    Improved upgrade script, added .profile for environment path on storage card.

--- files/root/upgrade_fromhelios.sh
+++ files/root/upgrade_fromhelios.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 scp -i ~/.ssh/id_rsa device@machine20.touchstar.com.au:/tftpboot/firmware$1.bin /tmp/firmware.bin
+if [ $? -ne 0 ]; then
+    echo "Unable to download firmware$1.bin from helios"
+    exit 1
+fi
+
+#################################################
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!!! This is a destructive operation !!!!"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo
+echo "The operating system will be upgraded with [firmware$1.bin]"
+echo
+read -p "Are you sure? (y/N) " -n 1 -r
+echo
+if ! echo "$REPLY" | grep -iq "^y"; then
+    rm /tmp/firmware.bin
+    exit 1
+fi
 sysupgrade $2 /tmp/firmware.bin
