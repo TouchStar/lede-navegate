f80c152537e069de0422499ccf22d432eed3380f
David Thornley <david.thornley@touchstargroup.com>
2016-11-24 17:04:09 +1100

    [ITL-36] Increased timeout between off/on (to 1 + 5 seconds) after continued testing on alternate hardware.

--- files/lib/touchstar/cellular_powercycle.sh
+++ files/lib/touchstar/cellular_powercycle.sh
@@ -4,6 +4,7 @@ BASEDIR=$(dirname "$0")
 # Cinterion PHP8 - power control - assumes "AT^SCFG=MEShutdown/OnIgnition,ON"
 # Here we assume the modem was powered for at least 5s.
 
+sleep 1.0
 $BASEDIR/cellular_poweroff.sh
-sleep 3.5
+sleep 5.0
 $BASEDIR/cellular_poweron.sh
--- files/lib/touchstar/cellular_poweroff.sh
+++ files/lib/touchstar/cellular_poweroff.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # see cellular_powercycle.sh
 echo 0 > /sys/class/gpio/gpio14/value
-sleep 2.3
+sleep 2.5
 echo 1 > /sys/class/gpio/gpio14/value
